09816bca29e660b0bf5c7f02b6e1628c4020ebf1
sky <854323752@qq.com>
2018-01-09 14:33:05 +0800

    修改adaptor.js

--- tools/adapter.js
+++ tools/adapter.js
@@ -1,56 +1,42 @@
 /**
- * Created by sky on 2016/12/20.
- * 适配手机屏幕
- * 注：标签的尺寸单位请使用 rem(根据html的font-size定义宽度)
- *    页面按照640px宽度定义，如果想要得到750px的宽度，只要修改成7.5rem即可，以此类推！(rem = px/100)
- */
+ * @author Sky
+ * @email lihaizh_cn@foxmail.com
+ * @create date 2018-01-02 05:17:38
+ * @modify date 2018-01-02 05:17:38
+ * @desc 适配手机屏幕
+ *  注：标签的尺寸单位请使用 rem(根据html的font-size定义宽度)
+ *     页面按照750px宽度定义，如果想要得到750px的宽度，只要修改成7.5rem即可，以此类推！(rem = px/100)
+ *     data-mw = '750' default
+*/
 
+/* eslint no-var: "off" */
+/* eslint func-names: "off" */
+/* eslint prefer-template: "off" */
+/* eslint-env es5 */
 (function (doc, win) {
-    var docEl = doc.documentElement,
-        docBody = doc.body,
-        maxWidth = docEl.dataset['mw'] || 750,
-        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-        // 当文档正在加载时,返回loading,
-        // 当文档结束渲染但内在资源正在加载时,返回interactive
-        // 当文档加载完成时,返回complete
-        readyRE = /complete|loaded|interactive/,
-        ratio = window.devicePixelRatio || 1,
-        retina = function () {
-            var metas, meta, i = 0;
-            if (window.devicePixelRatio) {
-                meta = document.createElement('meta');
-                meta.setAttribute('name', 'viewport');
-                meta.setAttribute('content', 'width=device-width, initial-scale=' + (1/ratio) + ', user-scalable=no');
-                metas = document.querySelectorAll('meta[name="viewport"]');
-                for (; i < metas.length; i++) {
-                    document.head.removeChild(metas[i]);
-                }
-                document.head.appendChild(meta);
-            }
-        },
-        recalc = function () {
-            var clientWidth;
-            if (win.innerWidth) {
-                clientWidth = win.innerWidth;
-            } else if (docEl && docEl.getBoundingClientRect) {
-                clientWidth = docEl.getBoundingClientRect().width;
-            } else {
-                clientWidth = docBody.getBoundingClientRect().width;
-            }
-            if (!clientWidth) return;
-            docEl.style.fontSize = 100 * (clientWidth / maxWidth) + 'px';
-        };
+  var docEl = doc.documentElement;
+  var maxWidth = docEl.dataset.mw || 750;
+  var resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize';
+  var readyRE = /complete|loaded|interactive/;
 
-    retina();
-    if (document.addEventListener) {
-        win.addEventListener(resizeEvt, recalc, false);
-        doc.addEventListener('DOMContentLoaded', recalc, false);
-    } else if (document.attachEvent) {
-        win.attachEvent(resizeEvt, recalc);
-        doc.attachEvent('onreadystatechange', function() {
-            if (readyRE.test(doc.readyState)) {
-                recalc();
-            }
-        });
-    }
-})(document, window);
+  function recalc() {
+    // 获取设备的宽度
+    var clientWidth = window.screen.width;
+    if (!clientWidth) return;
+    docEl.style.fontSize = 100 * (clientWidth / maxWidth) + 'px';
+    // window.devicePixelRatio是设备上物理像素和设备独立像素(device-independent pixels (dips))的比例。
+    // 公式就是: window.devicePixelRatio = 物理像素 / dips (非视网膜屏为1， 视网膜屏为2)
+    docEl.setAttribute('dpr', window.devicePixelRatio || 1);
+    doc.removeEventListener('DOMContentLoaded', recalc, false);
+  }
+
+  // Abort if browser does not support addEventListener
+  if (!doc.addEventListener) return;
+
+  win.addEventListener(resizeEvt, recalc, false);
+  if (readyRE.test(document.readyState) && document.body) {
+    recalc();
+  } else {
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+  }
+}(document, window));
--- tools/storage.js
+++ tools/storage.js
@@ -1,115 +1,135 @@
 /**
- * Created by sky on 16/10/26.
- * storage检测和封装
- */
+ * @author Sky
+ * @email lihaizh_cn@foxmail.com
+ * @create date 2018-01-09 01:14:05
+ * @modify date 2018-01-09 01:14:05
+ * @desc Storage封装
+*/
 
-;(function () {
-    var type = {
-        isObj: function (o) {
-            return Object.prototype.toString.call(o).slice(8, -1).toLowerCase() === 'object';
-        },
-        isArr: function (o) {
-            return o instanceof Array;
-        },
-        isStr: function (o) {
-            return typeof o === 'string';
-        },
-        isFunc: function (o) {
-            return typeof o === 'function';
-        }
-    };
+/* eslint no-var: "off" */
+/* eslint func-names: "off" */
+/* eslint object-shorthand: "off" */
+/* eslint comma-dangle: "off" */
+/* eslint prefer-rest-params: "off" */
+/* eslint no-plusplus: "off" */
+/* eslint no-restricted-syntax: "off" */
+/* eslint prefer-template: "off" */
+/* eslint-env es5 */
+(function () {
+  var type = {
+    isObj: function (o) {
+      return Object.prototype.toString.call(o).slice(8, -1).toLowerCase() === 'object';
+    },
+    isArr: function (o) {
+      return o instanceof Array;
+    },
+    isStr: function (o) {
+      return typeof o === 'string';
+    },
+    isFunc: function (o) {
+      return typeof o === 'function';
+    }
+  };
 
-    function parseJSON(data) {
-        try {
-            return type.isObj(data) || type.isArr(data) ? JSON.parse(data) : data;
-        } catch (ex) {
-            return data;
+  function parseJSON(data) {
+    try {
+      return type.isObj(data) || type.isArr(data) ? JSON.parse(data) : data;
+    } catch (ex) {
+      return data;
+    }
+  }
+
+  function stringify(data) {
+    try {
+      return type.isFunc(data) ? data : JSON.stringify(data);
+    } catch (ex) {
+      return data;
+    }
+  }
+
+  function DataBase(store) {
+    this.store = store;
+  }
+
+  DataBase.prototype = {
+    get: function () {
+      var i = 0;
+      var len;
+      var key = arguments[0];
+      var arr;
+      var result = '';
+      if (type.isStr(key)) {
+        result = parseJSON(this.store.getItem(key));
+      } else if (type.isArr(key)) {
+        arr = key;
+        key = null;
+        len = arr.length;
+        result = {};
+        for (; i < len; i++) {
+          key = arr[i];
+          if (type.isStr(key)) {
+            result[key] = parseJSON(this.store.getItem(key));
+          }
         }
+      }
+      return result;
+    },
+    set: function (arg1, arg2) {
+      var key;
+      var value;
+      var obj;
+      if (type.isStr(arg1)) {
+        key = arg1;
+        value = arg2;
+        this.store.setItem(key, stringify(value));
+      } else if (type.isObj(arg1)) {
+        obj = arg1;
+        for (key in obj) {
+          if (obj.hasOwnProperty(key)) {
+            value = stringify(obj[key]);
+            this.store.setItem(key, value);
+          }
+        }
+      }
+    },
+    remove: function (arg1) {
+      var i = 0;
+      var key;
+      var len = arguments.length;
+      if (len === 0) {
+        this.store.clear();
+      } else if (len === 1) {
+        key = arg1;
+        this.store.removeItem(key);
+      } else {
+        for (; i < len; i++) {
+          key = arguments[i];
+          this.store.removeItem(key);
+        }
+      }
+    },
+    clear: function () {
+      this.store.clear();
+    }
+  };
+
+  window.db = function (isForeverStore) {
+    var store = isForeverStore ? window.localStorage : window.sessionStorage;
+    var storageTest = 'storageTest_' + new Date().getTime();
+
+    if (store) {
+      try {
+        store.setItem(storageTest, storageTest);
+        store.removeItem(storageTest);
+      } catch (ex) {
+        console.error(ex);
+        alert('关闭浏览器的无痕模式试试吧~~');
+      }
+    } else {
+      alert('浏览器不支持' + (isForeverStore ? 'localStorage' : 'sessionStorage') + '哦!\n换个浏览器试试~~');
     }
 
-    function stringify(data) {
-        try {
-            return type.isFunc(data) ? data : JSON.stringify(data);
-        } catch (ex) {
-            return data;
-        }
-    }
+    return new DataBase(store);
+  };
 
-    function DataBase(store) {
-        this.store = store;
-    }
-
-    DataBase.prototype = {
-        get: function () {
-            var i = 0, len, key = arguments[0], arr, result = '';
-            if (type.isStr(key)) {
-                result = parseJSON(this.store.getItem(key));
-            } else if (type.isArr(key)) {
-                arr = key;
-                key = null;
-                len = arr.length;
-                result = {};
-                for (; i < len; i++) {
-                    key = arr[i];
-                    if (type.isStr(key)) {
-                        result[key] = parseJSON(this.store.getItem(key));
-                    }
-                }
-            }
-            return result;
-        },
-        set: function () {
-            var key, value, obj;
-            if (type.isStr(arguments[0])) {
-                key = arguments[0];
-                value = arguments[1];
-                this.store.setItem(key, stringify(value));
-            } else if (type.isObj(arguments[0])) {
-                obj = arguments[0];
-                for (key in obj) {
-                    if (obj.hasOwnProperty(key)) {
-                        value = stringify(obj[key]);
-                        this.store.setItem(key, value);
-                    }
-                }
-            }
-        },
-        remove: function () {
-            var i = 0, key, len = arguments.length;
-            if (len === 0) {
-                this.store.clear();
-            } else if (len === 1) {
-                key = arguments[0];
-                this.store.removeItem(key);
-            } else {
-                for (; i < len; i++) {
-                    key = arguments[i];
-                    this.store.removeItem(key);
-                }
-            }
-        },
-        clear: function () {
-            this.store.clear();
-        }
-    };
-
-    window.db = function (isForeverStore) {
-        var store = isForeverStore ? window.localStorage : window.sessionStorage,
-            storageTest = 'storageTest_' + new Date().getTime();
-
-        if (store) {
-            try {
-                store.setItem(storageTest, storageTest);
-                store.removeItem(storageTest);
-            } catch (ex) {
-                console.error(ex);
-                alert('关闭浏览器的无痕模式试试吧~~');
-            }
-        } else {
-            alert('浏览器不支持' + (isForeverStore ? 'localStorage' : 'sessionStorage') + '哦!\n换个浏览器试试~~');
-        }
-
-        return new DataBase(store);
-    };
-
-})();
+}());
