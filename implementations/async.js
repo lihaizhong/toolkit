503c2cfbbc903120d028a5f27885ddebd061e94f
sky <854323752@qq.com>
2020-09-20 02:43:55 +0800

    feat: 添加async实现

--- implementations/async.js
+++ implementations/async.js
@@ -1,13 +1,24 @@
+/**
+ * async函数的实现
+ *
+ * 1. async函数是Generator函数的语法糖，等同于自执行的Generator函数。
+ * 2. async函数返回一个新的Promise对象。
+ */
+
 function asyncToGeneratorNext (gen, resolve, reject, _next, _throw, key, arg) {
   try {
+    // 获取generator对象
     const info = gen[key](arg)
 
     if (info.done) {
+      // 完成所有断点时，执行成功操作
       resolve(info.value)
     } else {
+      // 递归调用断点，使用Promise，保持断点的连贯性
       Promise.resolve(info.value).then(_next, _throw)
     }
   } catch (e) {
+    // 某个断点报错时，执行失败操作
     reject(e)
   }
 }
